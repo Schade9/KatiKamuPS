d8ef986ad100573655aa2a66298da78184ca7ed9
Odeke12 <trevodex@gmail.com>
2021-12-30 15:03:28 +0300

    Class for creating teacher objects

---
+++ src/WinBuilder/Teacher.java
@@ -0,0 +1,15 @@
+package WinBuilder;
+
+public class Teacher {
+	String first_name, last_name, email ,password, subjects,classes;
+//	String classes[];
+	public Teacher(String fname,String lname,String email,String subjects, String password, String classes) {
+		this.first_name = fname;
+		this.last_name = lname;
+		this.email = email;
+		this.subjects = subjects;
+		this.password = password;
+		this.classes = classes;
+	}
+}
+ 
